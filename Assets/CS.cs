87ffd082bbf2c55e2030039c4f9c0971f3c41639
moole100 <phmi1621@naver.com>
2020-09-09 21:10:54 +0900

    Upload Unity file

---
+++ Assets/CS.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
