2a00b0db6a6e43fafb26109389633aed1f34999a
waixxt3213 <qnqn60360@gmail.com>
2020-09-23 22:31:58 +0900

    Create Initial Main Menu

--- Assets/CS.cs
+++ Assets/CS.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CS : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Default()
     {
         Screen.SetResolution(1920, 1080, true);
     }
@@ -13,6 +11,9 @@ public class CS : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
     }
 }
---
+++ Assets/MainMenu1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RunQuit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void GoOptionsScene()
+    {
+        SceneManager.LoadScene("Scenes/Scenes2");
+    }
+
+    public void GoMain_menuScene()
+    {
+        SceneManager.LoadScene("Scenes/Scenes1");
+    }
+
+    public void HD()
+    {
+        Screen.SetResolution(1280,720,false);
+    }
+}
