50661a0fcfca70c48e834b423fec2c9880f06cda
waixxt3213 <qnqn60360@gmail.com>
2020-09-25 20:30:38 +0900

    All convert UI TMP && Options Change

---
+++ Assets/Scripts/MM1_O_Graphics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MM1_O_Graphics : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public void WindowMode(int value)
+    {
+        switch (value)
+        {
+            case 0:
+            {
+                Screen.SetResolution (1920, 1080, true);
+                break;
+            }
+
+            case 1:
+            {
+                Screen.SetResolution(1280, 720, false);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RunQuit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void ChangeScene(string scenesName)
+    {
+        SceneManager.LoadScene(scenesName);
+    }
+
+    public void Start(string scenesName1) // but this only Debug
+    {
+        Debug.Log("Debugging" + scenesName1);
+        // SceneManager.LoadScene("scenesName");
+
+    }
+}
