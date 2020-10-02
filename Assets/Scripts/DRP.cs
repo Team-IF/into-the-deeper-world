aab23c1c6035c6805f65833452b5ac0b9f9c7b47
waixxt3213 <qnqn60360@gmail.com>
2020-10-02 13:29:41 +0900

    DRP work update

--- Assets/Scripts/DRP.cs
+++ Assets/Scripts/DRP.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using System;  
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Discord;
 
 public class DRP : MonoBehaviour
 {
-    private void Start()
+    public Discord.Discord discord;
+
+    void Start()
     {
+        discord = new Discord.Discord(756805917944578112, (UInt64)Discord.CreateFlags.Default);
         var now = DateTime.Now.ToLocalTime();
         var span = (now - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+        var activityManager = discord.GetActivityManager();
         var activity = new Discord.Activity
         {
             State = "Into the Deeper world",
@@ -23,12 +30,18 @@ public class DRP : MonoBehaviour
                 SmallText = null,
             },
         };
+        activityManager.UpdateActivity(activity, (result) =>
+        {
+            if (result == Discord.Result.Ok)
+            {
+                Debug.LogError("Everything is fine!");
+            }
+        });
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        var discord = new Discord.Discord(756805917944578112, (UInt64)Discord.CreateFlags.Default);
         discord.RunCallbacks();
     }
 }
